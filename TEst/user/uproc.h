6743b770be80cb627c253eba00debeb3eeb495d1
edgarzub <egarnica@miner.utep.edu>
2022-09-11 22:19:04 -0600

    Hello

---
+++ TEst/user/uproc.h
@@ -0,0 +1,13 @@
+/* 
+  * file uproc.h  with definition of user process structure
+*/
+enum uprocstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
+
+struct uproc {
+    int pid;                   // Process ID
+    enum uprocstate state;      // Process state
+    uint64 size;               // Size of process memory (bytes)
+    int ppid;                  // Parent ID
+    char name[16];             // Process command name
+};
+
