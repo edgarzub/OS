fc93f0110da1ca060d6bbacf9f8f4a7fa3461266
edgarzub <egarnica@miners.utep.edu>
2022-09-23 22:08:11 -0600

    Lab 2

---
+++ Lab 2/xv6-riscv-labs-lab2/kernel/pstat.h
@@ -0,0 +1,8 @@
+struct pstat {
+  int pid;     // Process ID
+  enum procstate state;  // Process state
+  uint64 size;     // Size of process memory (bytes)
+  int ppid;        // Parent process ID
+  char name[16];   // Parent command name
+  int cputime; // cpu time
+};
